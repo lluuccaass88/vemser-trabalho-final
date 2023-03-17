e0c2ea0236ffb9434c9f2c4d66223656e4a5e5a4
Marcklen <marcklen@icloud.com>
2023-03-17 11:43:30 -0300

    REFACT MotoristaController

--- src/main/java/br/com/logisticadbc/service/MotoristaService.java
+++ src/main/java/br/com/logisticadbc/service/MotoristaService.java
@@ -1,6 +1,45 @@
 package br.com.logisticadbc.service;
 
+import br.com.logisticadbc.dto.MotoristaCreateDTO;
+import br.com.logisticadbc.dto.MotoristaDTO;
+import br.com.logisticadbc.entity.MotoristaEntity;
+import br.com.logisticadbc.entity.enums.StatusUsuario;
+import br.com.logisticadbc.exceptions.RegraDeNegocioException;
+import br.com.logisticadbc.repository.MotoristaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MotoristaService {
 
+    private final MotoristaRepository motoristaRepository;
+    private final ObjectMapper objectMapper;
+
     // TODO quando for criar ou alterar o motorista , verificar o status
+
+    // TODO CRUD completo nome metodos -> CRIAR , EDITAR, LISTAR, DELETAR E BUSCARPORID
+
+    public List<MotoristaDTO> listar() {
+        return motoristaRepository
+                .findAll()
+                .stream()
+                .map(motorista -> objectMapper.convertValue(motorista, MotoristaDTO.class)).toList();
+    }
+
+    // o metodo buscar por id retornando o obj motorista é privado por expor a entidade
+    private MotoristaEntity buscarPorId(Integer id) throws RegraDeNegocioException{
+        return motoristaRepository.findById(id)
+                .orElseThrow(() -> new RegraDeNegocioException("Motorista não encontrado"));
+    }
+
+    public MotoristaDTO criar (MotoristaCreateDTO motoristaCreateDTO) {
+        MotoristaEntity motoristaEntity = objectMapper.convertValue(motoristaCreateDTO, MotoristaEntity.class);
+        motoristaEntity.setStatusUsuario(StatusUsuario.ATIVO);
+        motoristaRepository.save(motoristaEntity);
+        return objectMapper.convertValue(motoristaEntity, MotoristaDTO.class);
+    }
 }
