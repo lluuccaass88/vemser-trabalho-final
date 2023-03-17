a87e99fdaa93a3b78b39f3c598a7da1fbb07cd8d
Marcklen <marcklen@icloud.com>
2023-03-17 14:18:33 -0300

    REFACT MotoristaService

--- src/main/java/br/com/logisticadbc/dto/MotoristaDTO.java
+++ src/main/java/br/com/logisticadbc/dto/MotoristaDTO.java
@@ -6,8 +6,11 @@ import lombok.Data;
 @Data
 public class MotoristaDTO extends MotoristaCreateDTO {
 
-    @Schema(description = "Id do Motorista", example = "1")
+    @Schema(description = "Id do Usuário", example = "1")
     private Integer idUsuario;
-    @Schema(description = "Status do Motorista", example = "0 - ATIVO ou 1 - INATIVO")
+    @Schema(description = "Status do Usuário", example = "0 - ATIVO ou 1 - INATIVO")
     private Integer statusUsuario;
+
+    @Schema(description = "Status do Motorista", example = "0 - DISPONIVEL ou 1 - EM VIAGEM")
+    private Integer statusMotorista;
 }
