33061a97bfd64b9a562ab61765b51a7ce8b4b22a
Marcklen <marcklen@icloud.com>
2023-03-17 10:33:40 -0300

    Adiciona DTOs para MotoristaEntity

---
+++ src/main/java/br/com/logisticadbc/dto/MotoristaDTO.java
@@ -0,0 +1,11 @@
+package br.com.logisticadbc.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class MotoristaDTO extends MotoristaCreateDTO {
+
+    @Schema(description = "Id do Motorista", example = "1")
+    private Integer idUsuario;
+}
--- src/main/java/br/com/logisticadbc/service/MotoristaService.java
+++ src/main/java/br/com/logisticadbc/service/MotoristaService.java
@@ -1,4 +1,6 @@
 package br.com.logisticadbc.service;
 
 public class MotoristaService {
+
+    // TODO quando for criar ou alterar o motorista , verificar o status
 }
