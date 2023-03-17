fe9cc19c4f249ee232318258e151a74aec268458
Marcklen <marcklen@icloud.com>
2023-03-17 14:44:50 -0300

    REFACT MotoristaService CREATE

--- src/main/java/br/com/logisticadbc/dto/MotoristaDTO.java
+++ src/main/java/br/com/logisticadbc/dto/MotoristaDTO.java
@@ -1,5 +1,7 @@
 package br.com.logisticadbc.dto;
 
+import br.com.logisticadbc.entity.enums.StatusMotorista;
+import br.com.logisticadbc.entity.enums.StatusUsuario;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -9,8 +11,7 @@ public class MotoristaDTO extends MotoristaCreateDTO {
     @Schema(description = "Id do Usuário", example = "1")
     private Integer idUsuario;
     @Schema(description = "Status do Usuário", example = "0 - ATIVO ou 1 - INATIVO")
-    private Integer statusUsuario;
-
+    private StatusUsuario statusUsuario;
     @Schema(description = "Status do Motorista", example = "0 - DISPONIVEL ou 1 - EM VIAGEM")
-    private Integer statusMotorista;
+    private StatusMotorista statusMotorista;
 }
