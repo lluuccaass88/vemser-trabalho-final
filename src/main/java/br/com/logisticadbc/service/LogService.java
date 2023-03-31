851c80dcf285c7b2bab2503f40cd5fadec24ef67
Marcklen <marcklen@icloud.com>
2023-03-30 23:07:01 -0300

    adiciona log com info no mongo sobre criacao, abastecimento e inativacao de rotas

--- src/main/java/br/com/logisticadbc/service/LogService.java
+++ src/main/java/br/com/logisticadbc/service/LogService.java
@@ -1,11 +1,13 @@
 package br.com.logisticadbc.service;
 
 import br.com.logisticadbc.dto.out.LogDTO;
+import br.com.logisticadbc.entity.UsuarioEntity;
+import br.com.logisticadbc.entity.enums.TipoOperacao;
 import br.com.logisticadbc.entity.mongodb.LogEntity;
+import br.com.logisticadbc.exceptions.RegraDeNegocioException;
 import br.com.logisticadbc.repository.LogRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,12 +19,8 @@ public class LogService {
 
     private final LogRepository logRepository;
     private final ObjectMapper objectMapper;
+    private final UsuarioService usuarioService;
 
-    public void save(LogDTO logDTO) {
-        var log = new LogEntity();
-        BeanUtils.copyProperties(logDTO, log);
-        logRepository.save(log);
-    }
 
     // TODO - METODO CRIADO PORÉM NAO SEI SE SERÁ UTILIZADO
     public List<LogDTO> listAllLogs() {
@@ -31,5 +29,21 @@ public class LogService {
                 .stream()
                 .map(log -> objectMapper.convertValue(log, LogDTO.class))
                 .collect(Collectors.toList());
+    }
+
+    public void gerarLog(UsuarioEntity usuario, String descricao, TipoOperacao tipoOperacao) throws RegraDeNegocioException {
+        Integer idUsuario = usuario.getIdUsuario();
+        UsuarioEntity usuarioEntity = usuarioService.buscarPorId(idUsuario);
+
+        LogEntity log = new LogEntity();
+        log.setLoginOperador(usuarioEntity.getLogin());
+        log.setDescricao(descricao);
+        log.setTipoOperacao(tipoOperacao);
+
+        try {
+            logRepository.save(log);
+        } catch (Exception e) {
+            throw new RegraDeNegocioException("Aconteceu algum problema durante a criação do log.");
+        }
     }
 }
