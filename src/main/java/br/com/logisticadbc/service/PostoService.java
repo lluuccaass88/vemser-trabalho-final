4da27969e99eaf97189a3549b854466c44f45e8f
marimaccos <mariana_maccos@hotmail.com>
2023-04-01 15:03:54 -0300

    resolve conflitos

--- src/main/java/br/com/logisticadbc/service/PostoService.java
+++ src/main/java/br/com/logisticadbc/service/PostoService.java
@@ -104,7 +104,6 @@ public class PostoService {
         return postoRepository.findAll()
                 .stream()
                 .map(posto -> {
-//                   PostoDTO postoDTO = new PostoDTO(posto.getId(), posto.getNome(), posto.getLocation(), posto.getCidade(), posto.getValorCombustivel(), posto.getStatus());
                     PostoDTO postoDTO = objectMapper.convertValue(posto, PostoDTO.class);
                     return postoDTO;})
                 .toList();
